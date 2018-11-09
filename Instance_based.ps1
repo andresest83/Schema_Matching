0aa0798258aff7539b56a2b9ad9dfe8bca9e2069
Andres Estupinan <andrespra@gmail.com>
2018-11-09 16:32:52 +0100

    Created Instance_based script file and description

---
+++ Instance_based.ps1
@@ -0,0 +1,7 @@
+﻿# The script takes two pre-loaded schemas and compares them using a fixed relationship betweeen the fields of one
+# schema and the other.
+# The user is presented with the option of comparing such schemas using three different algorithms:
+# 1. Exact match
+# 2. Levenshtein distance
+# 3. Jaccard distance
+
